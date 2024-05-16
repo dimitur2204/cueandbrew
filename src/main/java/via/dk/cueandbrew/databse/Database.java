d015c910815b748352d3e9f6461deca99957fe7b
marcocimarius <mmarcoci55@gmail.com>
2024-05-16 15:51:18 +0200

    fak you

--- src/main/java/via/dk/cueandbrew/databse/Database.java
+++ src/main/java/via/dk/cueandbrew/databse/Database.java
@@ -8,7 +8,7 @@ public class Database {
     // You need to change the password and user to your own
     private static final String schema = "cueandbrew";
     private static final String url = "jdbc:postgresql://localhost:5432/cueandbrew?currentSchema=" + schema;
-    private static final String user = "dimitar.nizamov";
+    private static final String user = "postgres";
     private static final String password = "";
     public static Connection createConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
